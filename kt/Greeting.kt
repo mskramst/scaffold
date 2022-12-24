4ee78660b8d215ae778b536154d00161efa8cdc6
m <mskramst@gmail.com>
2022-12-24 13:17:54 -0800

    Added kotlin scaffold code

---
+++ kt/Greeting.kt
@@ -0,0 +1,7 @@
+package collections
+
+fun hello() {
+  println("This is a simple greeting!")
+  println("This is another greeting!")
+  println("This is a third message!")
+}
---
+++ kt/Main.kt
@@ -0,0 +1,7 @@
+package collections
+
+
+fun main() {
+
+  hello()
+}
